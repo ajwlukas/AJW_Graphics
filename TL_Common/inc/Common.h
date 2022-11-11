748db11a7e950e9c6f0a59c7314e5e29ca7ef227
ajwlukas <ajwlukas@gmail.com>
2022-11-11 18:14:48 +0900

    221111

---
+++ TL_Common/inc/Common.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include "Common/Common_typedef.h"
+#include "Common/Common_type.h"
+#include "Common/Common_prefix.h"
+
+#include "Common/DebugHelper.h"
+#include "Common/StringHelper.h"
+
+#include "Common/SingletonInstance.h"
---
+++ TL_Common/inc/Common/DebugHelper.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "Common_typedef.h"
+
+class COMMON_API DebugHelper
+{
+public:
+    /**
+     * @brief 형식화된 문자열을 debug console에 출력합니다.
+    */
+    template <typename ... Args>
+    static void PrintDebugString(const tstring& _format, Args ... _args);
+
+    /**
+     * @brief 형식화된 문자열을 debug console에 출력합니다.
+    */
+    template <typename ... Args>
+    static void PrintDebugString(tstring& _format, Args ... _args);
+
+    /**
+     * @brief 형식화된 문자열을 debug console에 출력합니다.
+    */
+    template <typename ... Args>
+    static void PrintDebugString(const TCHAR* _format, Args ... _args);
+
+    /**
+     * @brief 형식화된 문자열을 debug console에 출력합니다.
+    */
+    template <typename ... Args>
+    static void PrintDebugString(TCHAR* _format, Args ... _args);
+};
+
+#include "DebugHelper.inl"
